182cc84710f433b4fffda4961e76179477e13bd6
Samad Ahmed <samadahmed2019@gmail.com>
2020-03-28 01:54:05 -0500

    Built login/signup form

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function(req, res, next) {
+  res.render("index", {
+    title: "Pandemic Price Checker",
+    subtitle: "A free-to-use price-gouging reporting tool"
+  });
 });
 
 module.exports = router;
