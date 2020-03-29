c36e44e3ab14155da0484fae1327033bb18d33e1
Samad Ahmed <samadahmed2019@gmail.com>
2020-03-29 01:17:47 -0500

    Built login page hbs

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,18 @@ var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get("/", function(req, res, next) {
+router.get("/", (req, res, next) => {
   res.render("index", {
     title: "Pandemic Price Checker",
-    subtitle: "A free-to-use price-gouging reporting tool"
+    subtitle:
+      "A free-to-use price-gouging reporting tool to prevent unfair practices from unfair businesses.",
+    tagline:
+      "Prevent unjust retailers from taking advantage from you in real-time."
   });
+});
+
+router.get("/login", (req, res, next) => {
+  res.render("login", {});
 });
 
 module.exports = router;
