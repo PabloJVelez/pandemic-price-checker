658cf90f457b7c1b8919c8bf6e44b016efa41d90
Samad Ahmed <samadahmed2019@gmail.com>
2020-04-04 23:20:30 -0500

    Link non-user data submission pages

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ router.get("/", (req, res, next) => {
     subtitle:
       "A free-to-use price-gouging reporting tool to prevent unfair practices from unfair businesses.",
     tagline:
-      "Prevent unjust retailers from taking advantage from you in real-time."
+      "Prevent unjust retailers from taking advantage from you in real-time.",
   });
 });
 
