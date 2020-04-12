4a8169f27fc2cf6e8490b80a82bd42a72e52ef91
Samad Ahmed <samadahmed2019@gmail.com>
2020-04-12 00:56:43 -0500

    Change navbar item order; add route for logout

--- routes/priceChart.js
+++ routes/priceChart.js
@@ -2,9 +2,12 @@ var express = require("express");
 var router = express.Router();
 
 const price_chart_controller = require("../controllers/priceChartController");
+const logout_controller = require("../controllers/logoutController");
 
 router.get("/price-chart", price_chart_controller.price_chart_get);
 
 router.post("/price-chart", price_chart_controller.price_chart_post);
+
+router.post("/price-chart/logout", logout_controller.logout_post);
 
 module.exports = router;
